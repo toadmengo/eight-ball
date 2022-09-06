e66ed366eb890ee02b6f47197d0d8d567c9d04fc
Todd Meng <tftmeng@gmail.com>
2022-09-06 15:23:57 -0700

    fixe

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ api = Api(app)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
-    return send_from_directory(app.static_folder,'./js/build/index.html')
+    return send_from_directory(app.static_folder,'index.html')
 
 api.add_resource(APIHandler, '/ask')
