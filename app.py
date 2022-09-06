31cc2ae577496fe6dc1b8d97db2742d6c5df6ebd
Todd Meng <tftmeng@gmail.com>
2022-09-06 14:31:06 -0700

    first deployment

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS #comment this on deployment
+# from flask_cors import CORS #comment this on deployment
 from api.Api import APIHandler
 
 app = Flask(__name__, static_url_path='', static_folder='js/build')
-CORS(app) #comment this on deployment
+# CORS(app) #comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
