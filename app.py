a9d8e356a1c99e41719600b98d94ceebad90619a
Todd Meng <tftmeng@gmail.com>
2022-09-06 15:55:05 -0700

    send_from

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask_restful import Api, Resource, reqparse
 # from flask_cors import CORS #comment this on deployment
 from api.Api import APIHandler
 
-app = Flask(__name__, static_url_path='', static_folder='js/build')
+app = Flask(__name__, static_folder='js/build')
 # CORS(app) #comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
     print("Log1")
-    print(app.send_static_file('index.html'))
+    print(send_from_directory(app.static_folder, 'index.html'))
     print("Log2")
-    return app.send_static_file('index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
 api.add_resource(APIHandler, '/ask')
