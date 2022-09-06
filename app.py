dcecec3a2f943fc4e4ffa80147ed6b975b05a119
Todd Meng <tftmeng@gmail.com>
2022-09-06 15:35:01 -0700

    fix

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from flask_restful import Api, Resource, reqparse
 # from flask_cors import CORS #comment this on deployment
 from api.Api import APIHandler
 
-app = Flask(__name__, static_url_path='', static_folder='js/build')
+app = Flask(__name__, static_url_path='', static_folder='./js/build')
 # CORS(app) #comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
-    print("abcdxp")
     return send_from_directory(app.static_folder,'index.html')
 
 api.add_resource(APIHandler, '/ask')
